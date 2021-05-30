7a6996da87b54a782fcb65b5eb72382a2dc61c85
VanRar <vitalikf7@gmail.com>
2021-05-30 11:01:15 +0300

    первое и второе задания

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,24 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+
+        //первое задание
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "src/main/java/data.csv";
         List<Employee> list = parseCSV(columnMapping, fileName);
@@ -21,7 +30,68 @@ public class Main {
         String json = listToJson(list);
         String fileNameJSON = "src/main/java/data.json";
         writeString(json, fileNameJSON);
+
+        //второе задание
+        List<Employee> listXML = parseXML("src/main/java/data.xml");
+
+        String jsonXML = listToJson(list);
+        String fileNameJSONXML = "src/main/java/data2.json";
+        writeString(jsonXML, fileNameJSONXML);
+
     }
+
+    private static List<Employee> parseXML(String pathName) throws ParserConfigurationException, IOException, SAXException {
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File(pathName));
+        Node root = doc.getDocumentElement();
+        System.out.println("Корневой элемент: " + root.getNodeName());
+
+        return read(root);
+    }
+
+    private static List<Employee> read(Node node) {
+        NodeList nodeList = node.getChildNodes();
+        List<Employee> employeeList = new LinkedList<>();
+        int count = -1;
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node_ = nodeList.item(i);
+            if (Node.ELEMENT_NODE != node_.getNodeType() || node_.getTextContent().isEmpty() || node_.getTextContent().equals("\n")) {
+                continue;
+            }
+            if(node_.getNodeName().equals("employee")){
+                employeeList.add(new Employee());
+                count++;
+            }
+            NodeList nodeList1 = node_.getChildNodes();
+            for(int j = 0; j< nodeList1.getLength(); j++){
+                Node node2 = nodeList1.item(j);
+
+                if(node2.getNodeName().equals("id")){
+                   employeeList.get(count).id = Long.parseLong(node2.getTextContent());
+                }
+                if(node2.getNodeName().equals("firstName")){
+                    employeeList.get(count).firstName = node2.getTextContent();
+                }
+                if(node2.getNodeName().equals("lastName")){
+                    employeeList.get(count).lastName = node2.getTextContent();;
+                }
+                if(node2.getNodeName().equals("country")){
+                    employeeList.get(count).country = node2.getTextContent();
+                }
+                if(node2.getNodeName().equals("age")){
+                    employeeList.get(count).age = Integer.parseInt(node2.getTextContent());
+                }
+
+            }
+        }
+        //проверим
+        System.out.println(employeeList);
+        return  employeeList;
+    }
+
 
     private static void writeString(String json, String fileNameJSON) {
         try (FileWriter file = new
